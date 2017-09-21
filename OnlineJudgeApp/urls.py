eb76d09d58d4bf10360b1c8bd18dfddfd2e4251b
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2017-09-21 08:42:40 +0530

    Added front-end for contests, standings, settings, login, dashboard. Integrated DataTable.

--- OnlineJudgeApp/urls.py
+++ OnlineJudgeApp/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^$',views.login,name='login'),
 	url(r'^dashboard/$',views.dashboard,name='dashboard'),
 	url(r'^contests/$',views.contests,name='contests'),
 	url(r'^problemset/$',views.problemset,name='problemset'),
 	url(r'^settings/$',views.settings,name='settings'),
+	url(r'^logout/$',views.logout,name='logout'),
+	url(r'^contests/(?P<pk>\d+)/standings/',views.standings,name='standings'),
+	url(r'^contests/(?P<pk>\d+)/problems/',views.problems,name='problems'),
 ]
--- OnlineJudgeApp/views.py
+++ OnlineJudgeApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 # Create your views here.
 def dashboard(request):
 	return render(request, 'OnlineJudgeApp/dashboard.htm',{})
@@ -9,3 +10,11 @@ def problemset(request):
 	return render(request, 'OnlineJudgeApp/problemset.htm',{})
 def settings(request):
 	return render(request, 'OnlineJudgeApp/settings.htm',{})
+def login(request):
+	return render(request, 'OnlineJudgeApp/login.htm',{})
+def logout(request):
+	return HttpResponseRedirect(reverse('login'))
+def standings(request,pk):
+	return render(request, 'OnlineJudgeApp/standings.htm',{})
+def problems(request,pk):
+	return render(request, 'OnlineJudgeApp/problems.htm',{})
